6b4bc4e798b7dc049d50df8a881a4e4b4c10dc3b
Guillermo Armero <garmero1997@gmail.com>
2026-01-22 12:01:26 +0100

    Manejar los dos endpoints en la peticion.

--- RetrieveTEMP.js
+++ RetrieveTEMP.js
@@ -5,11 +5,17 @@ return TempF;
 
 async function RecuperarTemperatura (City,Type){
 
+    const apiKey="eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJnYXJtZXJvMTk5N0BnbWFpbC5jb20iLCJqdGkiOiI4MzhlZWNhMC04YzEzLTRlODktOTA0Ny1kYjcyMGVkMzI1YmMiLCJpc3MiOiJBRU1FVCIsImlhdCI6MTc2OTAwNDA2OCwidXNlcklkIjoiODM4ZWVjYTAtOGMxMy00ZTg5LTkwNDctZGI3MjBlZDMyNWJjIiwicm9sZSI6IiJ9.cMDDEJHTJw73QuFwUHpDC8EXdT2Tds-9Pri2l7HUCK4";
+    let EstacionID="3195"
     //Aqui ira la llamada get a la API de la amemet.
-    const response = await fetch("https://opendata.aemet.es/opendata/sh/31f48227");
-    const data = await response.json();
-    const TiempoHoras =data.map(item =>new ObjTiempo(item));
-    console.log(TiempoHoras);
+    const FirstResponse = await fetch(`https://opendata.aemet.es/opendata/api/observacion/convencional/datos/estacion/${EstacionID}?api_key=${apiKey}`);
+    const FirstData = await FirstResponse.json();
+
+
+    const RealResponse = await fetch(FirstData.datos);
+    const RealData = await RealResponse.json();
+    const TiempoHoras =RealData.map(item =>new ObjTiempo(item));
+    console.log(TiempoHoras[1]);
     //Convertimos a Celsius y Fharenheit segun nos mande el parametro
 };
 
